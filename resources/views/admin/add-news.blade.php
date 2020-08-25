ca3752386141d2ea1c5e473b4ae85e7a649261ad
Vasilii Sytchev <sychev.v@gmail.com>
2020-08-25 18:38:10 +0300

    Revert "вв"
    This reverts commit 54ba7333541f8a08dcdab12ab828a0da5d345594.

--- resources/views/admin/add-news.blade.php
+++ resources/views/admin/add-news.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.index')
 @section('admin-actions')
-    <form action="@if(isset($news)) {{ route('admin.Update-News', $news) }} @else {{ route('admin.Create-News') }} @endif" method="post" class="addNews" enctype="multipart/form-data">
+    <form action="{{ route('admin.Add-News') }}" method="post" class="addNews" enctype="multipart/form-data">
         @csrf
         <div class="input-group">
             <div class="input-group mb-3">
@@ -9,7 +9,7 @@
                 </div>
                 <select name="category" id="newsCategory" class="form-control">
                     @forelse ($categories as $category)
-                        <option @if ($category['id'] == old('category')) selected @endif value="{{ $category['id'] }}">{{ $category['name'] }}</option>
+                        <option @if ($category->id == old('category')) selected @endif value="{{ $category->id }}">{{ $category->name }}</option>
                     @empty
                         <option value="#">Категорий нет</option>
                     @endforelse
@@ -19,20 +19,16 @@
                 <div class="input-group-prepend">
                   <span class="input-group-text input-group-text_width" id="basic-addon1">Заголовок новости</span>
                 </div>
-                <input type="text" name="news-name" class="form-control" aria-label="Заголовок" aria-describedby="basic-addon1" value="{{ isset($news->title) ? $news->title : old('news-name') }}">
+                <input type="text" name="news-name" class="form-control" aria-label="Заголовок" aria-describedby="basic-addon1" value={{ old('news-name') }}>
             </div>
-            <div class="input-group mb-3 textAreaHeight">
+            <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text input-group-text_width">Текст новости</span>
                 </div>
-                <textarea class="form-control" name="news-text" aria-label="Текст новости">{{ isset($news->text) ? $news->text : old('news-text') }}</textarea>
+                <textarea class="form-control" name="news-text" aria-label="Текст новости">{{ old('news-text') }}</textarea>
             </div>
             <div class="input-group mb-3">
                 <input type="file" name="newsImage">
-            </div>
-            <div class="input-group mb-3 form-check">
-                <input @if (old('isPrivate') == 1 || isset($news) && $news->isPrivate == 1) checked @endif name="isPrivate" class="form-check-input" type="checkbox" id="newsPrivate">
-                <label class="form-check-label" for="newsPrivate">Новость для зарегистрированных пользователей?</label>
             </div>
             <div class="input-group mb-3">
                 <button type="submit" class="btn btn-dark">Отправить</button>
