9e3f410bfb0332d6818d91a149c58a22569bf483
Vasilii Sytchev <sychev.v@gmail.com>
2020-08-25 18:00:52 +0300

    One more

---
+++ resources/views/news/newsCategories.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.page')
+@section('content')
+    <h2>
+        Категории
+    </h2>
+    <ul class="newsCategories">
+        @forelse($categories as $item)
+            <li>
+                <a href="{{ route('news.CategoryByName', $item->slug) }}/">{{ $item->name }}</a>
+            </li>
+        @empty
+            Здесь пока нет ни одной категории
+        @endforelse
+    </ul>
+@endsection
